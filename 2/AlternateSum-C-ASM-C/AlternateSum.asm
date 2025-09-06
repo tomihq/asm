e51f4ed887a7cd72729262e405b118e43effb71c
tomihq <hernandeztomas584@gmail.com>
2025-09-06 17:44:25 -0300

    add: prologo, epilogo y mantener registros no volatiles

--- 2/AlternateSum-C-ASM-C/AlternateSum.asm
+++ 2/AlternateSum-C-ASM-C/AlternateSum.asm
@@ -2,9 +2,18 @@
 global alternate_sum_4_using_c
 extern sumar_c
 extern restar_c
-; Entrada: x1=x2=x3=x4 en registros (EDI, ESI, EDX, ECX)
+; Entrada: x1=x2=x3=x4 en registros (EDI, ESI, EDX, ECX). Como me llaman desde C, los registros volátiles que tengo que preservar son RDI y RSI
 ; Salida: EAX = resultado final
+; Siempre que llego estoy desalineado porque me alineo a 16 antes de saltar. Tons despues de saltar estoy a 8.
 section .text
 
 alternate_sum_4_using_c:
+    push rbp ;aca me alinee a 16 de vuelta.
+    mov rbp, rsp; seteo el base pointer
+    push rdi
+    push rsi 
+
+    pop rsi
+    pop rdi
+    pop rbp 
     ret
