a91e2e492a8bce8e2881a50c07188cb65af71d77
tomihq <hernandeztomas584@gmail.com>
2025-09-08 15:22:41 -0300

    add: ready ex 3

--- 3/Estructuras.asm
+++ 3/Estructuras.asm
@@ -11,10 +11,10 @@ PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
 PACKED_NODO_OFFSET_ARREGLO EQU 9
 PACKED_NODO_OFFSET_LONGITUD EQU 17
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+PACKED_NODO_SIZE EQU 21
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+PACKED_LISTA_OFFSET_HEAD EQU 0
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
@@ -51,7 +51,9 @@ cantidad_total_de_elementos:
 		ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
-;registros: lista[?]
+;registros:
+; lista -> RDI (es un puntero)
+; es igual al anterior pero ahora el offset esta packed.
 cantidad_total_de_elementos_packed:
 	push rbp
 	mov rbp, rsp 
