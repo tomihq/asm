3a79c59574898e13b167e3dc6ad2a575f798058d
tomihq <hernandeztomas584@gmail.com>
2025-09-08 15:12:39 -0300

    add: test_cantidad_total_de_elementos

--- 3/Estructuras.asm
+++ 3/Estructuras.asm
@@ -2,11 +2,11 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
 PACKED_NODO_OFFSET_NEXT EQU 8
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
 PACKED_NODO_OFFSET_ARREGLO EQU 8
@@ -29,9 +29,26 @@ global cantidad_total_de_elementos_packed
 
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
-;registros: lista[?]
+;registros: 
+;lista -> RDI (es un puntero)
+;como el resultado es un uint32_t el resultado lo devuelvo por EAX.
+;lo primero que tengo que hacer es pararme en la cabeza de la lista. El puntero es un nodo_t* head, lo cual al ser un puntero ocupa 64 bits. Me basta con pararme en 0 y eso me va a traer el next, la categoria, el arreglo que tiene y la longitud. 
 cantidad_total_de_elementos:
-	ret
+	push rbp
+	mov rbp, rsp 
+	xor eax, eax ; blanqueo eax para usarlo como acumulador y devolver la respuesta
+	mov rbx, [rdi] ; accedo al primer nodo de la lista. rbx apunta a {offset 0 en nodo_t}
+	
+	.ciclo: 
+		cmp rbx, 0
+		je .fin
+		add eax, DWORD[rbx + NODO_OFFSET_LONGITUD] ;hay un nodo en la lista.
+		mov rbx, [rbx] ;desreferencio el proximo nodo (offset 0)
+		jmp .ciclo
+	
+	.fin: 
+		pop rbp
+		ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
 ;registros: lista[?]
--- 3/main.c
+++ 3/main.c
@@ -8,6 +8,31 @@
 #include "Estructuras.h"
 
 int main() {
-	/* Acá pueden realizar sus propias pruebas */
+	
+	uint32_t arr[] = {1, 2, 3};
+    uint8_t categoria = 1;
+
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+    nodo->next = NULL;
+    nodo->categoria = categoria;
+    nodo->arreglo = arr;
+    nodo->longitud = sizeof(arr) / sizeof(arr[0]);
+
+	nodo_t* nodo_2 = malloc(sizeof(nodo_t));
+	nodo_2->next = NULL;
+    nodo_2->categoria = categoria;
+    nodo_2->arreglo = arr;
+    nodo_2->longitud = sizeof(arr) / sizeof(arr[0]);
+
+	nodo->next = nodo_2;
+
+
+    lista_t* lista = malloc(sizeof(lista_t));
+    lista->head = nodo;
+
+	cantidad_total_de_elementos(lista);
+
+    free(nodo);
+    free(lista);
 	return 0;
 }
