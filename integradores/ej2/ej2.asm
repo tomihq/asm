1ec0254672b28222a3f126d7fc69bcc2231d6ba6
tomihq <hernandeztomas584@gmail.com>
2025-09-14 13:12:09 -0300

    add: listo offsets

---
+++ integradores/ej2/ej2.asm
@@ -0,0 +1,63 @@
+extern malloc
+
+section .rodata
+; Acá se pueden poner todas las máscaras y datos que necesiten para el ejercicio
+
+section .text
+; Marca un ejercicio como aún no completado (esto hace que no corran sus tests)
+FALSE EQU 0
+; Marca un ejercicio como hecho
+TRUE  EQU 1
+
+; Marca el ejercicio 1A como hecho (`true`) o pendiente (`false`).
+;
+; Funciones a implementar:
+;   - optimizar
+global EJERCICIO_2A_HECHO
+EJERCICIO_2A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+
+; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
+;
+; Funciones a implementar:
+;   - contarCombustibleAsignado
+global EJERCICIO_2B_HECHO
+EJERCICIO_2B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+
+; Marca el ejercicio 1C como hecho (`true`) o pendiente (`false`).
+;
+; Funciones a implementar:
+;   - modificarUnidad
+global EJERCICIO_2C_HECHO
+EJERCICIO_2C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+
+;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
+; Completar las definiciones (serán revisadas por ABI enforcer):
+ATTACKUNIT_CLASE EQU 0
+ATTACKUNIT_COMBUSTIBLE EQU 12
+ATTACKUNIT_REFERENCES EQU 14
+ATTACKUNIT_SIZE EQU 16
+
+global optimizar
+optimizar:
+	; Te recomendamos llenar una tablita acá con cada parámetro y su
+	; ubicación según la convención de llamada. Prestá atención a qué
+	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
+	;
+	; r/m64 = mapa_t           mapa
+	; r/m64 = attackunit_t*    compartida
+	; r/m64 = uint32_t*        fun_hash(attackunit_t*)
+	ret
+
+global contarCombustibleAsignado
+contarCombustibleAsignado:
+	; r/m64 = mapa_t           mapa
+	; r/m64 = uint16_t*        fun_combustible(char*)
+	ret
+
+global modificarUnidad
+modificarUnidad:
+	; r/m64 = mapa_t           mapa
+	; r/m8  = uint8_t          x
+	; r/m8  = uint8_t          y
+	; r/m64 = void*            fun_modificar(attackunit_t*)
+	ret
