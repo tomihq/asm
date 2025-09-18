161b033b2f69b12e95c087d07125efd40caab803
tomihq <hernandeztomas584@gmail.com>
2025-09-18 13:41:25 -0300

    add: listo 1b

--- integradores/ej4/ej4b/ej4b.c
+++ integradores/ej4/ej4b/ej4b.c
@@ -4,5 +4,25 @@
 
 // OPCIONAL: implementar en C
 void invocar_habilidad(void* carta_generica, char* habilidad) {
-	card_t* carta = carta_generica;
+	 if (!carta_generica || !habilidad) return;
+
+    card_t* carta = (card_t*) carta_generica;
+
+    for (int i = 0; i < carta->__dir_entries; i++) {
+        directory_entry_t* actual_entry = carta->__dir[i];
+        if (!actual_entry) continue;
+
+        if (strcmp(actual_entry->ability_name, habilidad) == 0) {
+            ability_function_t* ability_ptr = (ability_function_t*) actual_entry->ability_ptr;
+            ability_ptr(carta_generica); 
+            return;
+        }
+    }
+
+    card_t* arquetipo = (card_t*) carta->__archetype;
+    if (arquetipo) {
+        invocar_habilidad(arquetipo, habilidad);
+    }
+
+
 }
